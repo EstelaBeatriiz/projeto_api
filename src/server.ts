bbcd95f3139a55c32cb2a9a9e62869db7425f478
Estela Beatriz <estelabeatriz149@gmail.com>
2026-06-17 20:04:50 -0300

    pasta user criada, conectando ao server.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { timeStamp } from 'node:console';
+import { User } from './user';
+
 
 const app = express();
 const PORT = 3000;
@@ -7,11 +9,25 @@ const PORT = 3000;
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({ message: 'Hello Word',
+  res.json({ message: 'Bem-vindo a API de Usuários',
     timeStamp: new Date().toISOString(),
-   });
-});
+    status: 'API funcionando!'
+   })
+})
+
+app.get('/users', (req: Request, res: Response) => {
+  const user = new User('estela', 'estela@gmail.com', '123')
+
+  console.log(user.verificarSenha('123'))
+  
+  res.json({
+    message: `Dados usuario: ${user.nome}`,
+    timeStamp: new Date().toISOString(),
+    user: user,
+    status: 'API funcionando!'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
+})
